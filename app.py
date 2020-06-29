49c603f31ea33a09cff962371317dfa81a4aca25
sivarajp <sivathevan@gmail.com>
2020-06-29 16:31:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,14 @@
 from flask import Flask
 import emoji
 import pyjokes
-​
+
 app=Flask(__name__)
-​
+
 @app.route('/')
 def hello():
   return pyjokes.get_joke()
   
-​
+
 @app.route('/fun')
 def fun():
   return emoji.emojize('Python development with TKG & Skaffold is :thumbs_up:')
